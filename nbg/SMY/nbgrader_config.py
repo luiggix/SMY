5861010aa91dc2f49813ea23265daaaafe359081
luiggix <luiggix@gmail.com>
2026-09-24 00:50:06 +0000

    clean

--- nbg/SMY/nbgrader_config.py
+++
@@ -1,9 +0,0 @@
-c = get_config() 
-
-########################################
-# MACTI: nbgrader_config.py 
-########################################
-c.CourseDirectory.course_id = "SMY" 
-c.IncludeHeaderFooter.header = "source/header.ipynb" 
-c.Exchange.root = "/usr/local/share/nbgrader/exchange" 
-c.CourseDirectory.root = "/home/jovyan/SMY/nbg/SMY" 
